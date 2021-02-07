9386bc6f6401142720bf261b2d0f1ea77d1e95f4
U-UBIMICRO\Stavros <stavros@ubinet.gr>
2021-02-07 18:11:01 +0100

    tiny-dnn model weight parser

---
+++ tiny-dnn-weights-parser.py
@@ -0,0 +1,19 @@
+# Tiny-DNN model weights parser - tiny-dnn-weight-parser.py - @Stavros Kalapothas 2021
+
+import json
+
+with open('c:/Users/Stavros/Downloads/LeNet-model.json') as f:
+  data = json.loads(f.read())
+
+count = 0   # debug counter
+
+for key, value in data.items():
+    #print(key)
+    if 'value' in key:
+        if 'type' in data[key]:
+            types = ('fully_connected', 'conv')
+            if any(t in data[key]['type'] for t in types):
+              #count +=1  debug
+              #print(count)
+              #for i in data[key]['value0']:
+              print(data[key]['value0'])
